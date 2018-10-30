76735a06c0bb4abccac4cf7183c685b3bfc1db43
gvaibhav182 <34602511+gvaibhav182@users.noreply.github.com>
2018-10-30 22:02:53 +0530

    Add files via upload

---
+++ images/index.php
@@ -0,0 +1,17 @@
+<?php
+echo'Error 404';
+?>
+<html>
+<head>
+<title>
+  Error Page
+</title>
+</head>
+<body>
+  <h1>Access Denied!!!</h1>
+  <h2>Page Redirect</h2>
+</body>
+</html>
+<?php
+echo "<script> alert('Redirecting............'); window.location.href='../index.html'; </script>" ;
+?>
